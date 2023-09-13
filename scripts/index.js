1b729ade92c40fadbbbf75a93c5a2c1b4ed654b6
Yury Bursian <dab180996@gmail.com>
2023-09-13 13:34:45 +0300

    JS array

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+const initialCards = [
+  {
+    title: "Architecture geometry",
+    link: "https://imgur.com/iX4Y4G7",
+  },
+  {
+    title: "Lost in the Wild",
+    link: "https://imgur.com/eW8KDHi",
+  },
+  {
+    title: "Tretyakov gallery",
+    link: "https://imgur.com/VfwhaLy",
+  },
+  {
+    title: "Construction grid",
+    link: "https://imgur.com/hEp4YXh",
+  },
+  {
+    title: "Church window",
+    link: "https://imgur.com/2eVFSi8",
+  },
+  {
+    title: "Stairs",
+    link: "https://imgur.com/MMY1UjL",
+  },
+];
+console.log(initialCards);
