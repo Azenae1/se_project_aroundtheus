3f54a48fe9ce88aff6bcc015a31b67f210396a9d
Yury Bursian <dab180996@gmail.com>
2023-09-13 14:09:10 +0300

    Stage-4.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,26 @@
 const initialCards = [
   {
-    title: "Architecture geometry",
+    name: "Architecture geometry",
     link: "https://imgur.com/xyupL3w",
   },
   {
-    title: "Lost in the Wild",
+    name: "Lost in the Wild",
     link: "https://imgur.com/49utHPV",
   },
   {
-    title: "Tretyakov gallery",
+    name: "Tretyakov gallery",
     link: "https://imgur.com/Pct2VsM",
   },
   {
-    title: "Construction grid",
+    name: "Construction grid",
     link: "https://imgur.com/pRck4w7",
   },
   {
-    title: "Church window",
+    name: "Church window",
     link: "https://imgur.com/pxdVMpn",
   },
   {
-    title: "Stairs",
+    name: "Stairs",
     link: "https://imgur.com/FYpxoNh",
   },
 ];
