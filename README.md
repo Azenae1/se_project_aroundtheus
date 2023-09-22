511d425192a6b362ff1899fb6de8260b5cff480a
Yury Bursian <dab180996@gmail.com>
2023-09-22 19:49:10 +0300

    Stage-4.4.3

--- README.md
+++ README.md
@@ -1,24 +1,22 @@
 # Project 3: Around The U.S.
 
-### Overview  
+### Overview
 
-* Intro  
-* Figma  
-* Images
-* This site represents responsive design suitable for both desktop and mobile devices starting from a 320p resolution.
-  
+- Intro
+- Figma
+- Images
+- This site represents responsive design suitable for both desktop and mobile devices starting from a 320p resolution.
+
 **Intro**
-  
-This project is made so all the elements are displayed correctly on popular screen sizes. We recommend investing more time in completing this project, since it's more difficult than previous ones.  
-  
-**Figma**  
-  
-* [Link to the project on Figma](https://www.figma.com/file/ii4xxsJ0ghevUOcssTlHZv/Sprint-3%3A-Around-the-US?node-id=0%3A1)  
-  
-**Images**  
-  
-The way you'll do this at work is by exporting images directly from Figma — we recommend doing that to practice more. Don't forget to optimize them [here](https://tinypng.com/), so your project loads faster. 
+
+This project is made so all the elements are displayed correctly on popular screen sizes.
+
+**New features**
+Partly working Profile editing function, cards are now not hardly coded in HTML.
+
+**Figma**
+
+- [Link to the project on Figma](https://www.figma.com/file/ii4xxsJ0ghevUOcssTlHZv/Sprint-3%3A-Around-the-US?node-id=0%3A1)
 
 **URL**
 https://azenae1.github.io/se_project_aroundtheus/
-
--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 const initialCards = [
   {
     name: "Architecture geometry",
-    link: "https://imgur.com/xyupL3w",
+    link: "https://i.pinimg.com/originals/24/ae/fe/24aefe8f2484c22d981e0558727b0652.png",
   },
   {
     name: "Lost in the Wild",
-    link: "https://imgur.com/49utHPV",
+    link: "https://i.pinimg.com/originals/4d/7c/ac/4d7cacba2ab863cc401f5c91d656999c.png",
   },
   {
-    name: "Tretyakov gallery",
-    link: "https://imgur.com/Pct2VsM",
+    name: "Light in the night",
+    link: "https://i.pinimg.com/originals/08/ab/e5/08abe5b23ea161c330f040785ee21f3f.png",
   },
   {
     name: "Construction grid",
-    link: "https://imgur.com/pRck4w7",
-  },
-  {
-    name: "Church window",
-    link: "https://imgur.com/pxdVMpn",
+    link: "https://i.pinimg.com/originals/dc/14/be/dc14be6b5cf38d516aa0740653fcbcb6.png",
   },
   {
     name: "Stairs",
-    link: "https://imgur.com/FYpxoNh",
+    link: "https://i.pinimg.com/originals/04/59/24/045924c2685a15901f44dcc559942afd.png",
+  },
+  {
+    name: "Church window",
+    link: "https://i.pinimg.com/originals/7a/89/e4/7a89e4ded58bb865c17d03a4bb0c6a14.png",
   },
 ];
 
@@ -37,9 +37,22 @@ const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
 const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+const cardListEl = document.querySelector(".cards__list");
 
 function toggleModalVisibility(modalWindow) {
   modalWindow.classList.toggle("modal_opened");
+}
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardNameEl = cardElement.querySelector(".card__name");
+
+  cardImageEl.src = cardData.link;
+  cardImageEl.alt = cardData.name;
+  cardNameEl.textContent = cardData.name;
+  return cardElement;
 }
 
 profileEditBtn.addEventListener("click", () => {
@@ -51,7 +64,6 @@ profileEditBtn.addEventListener("click", () => {
 
 profileCloseBtn.addEventListener("click", () => {
   toggleModalVisibility(profileEditModal);
-  // profileEditModal.classList.remove("modal_opened");
 });
 
 profileEditForm.addEventListener("submit", (e) => {
@@ -60,4 +72,9 @@ profileEditForm.addEventListener("submit", (e) => {
   profileDescription.textContent = profileDescriptionInput.value;
 
   toggleModalVisibility(profileEditModal);
+});
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEl.append(cardElement);
 });
