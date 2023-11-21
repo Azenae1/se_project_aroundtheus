6c46720d8b33077abd8cbb7609e2fa047dbd174f
Yury Bursian <dab180996@gmail.com>
2023-11-21 16:37:46 +0200

    Card class modified

--- components/Card.js
+++ components/Card.js
@@ -6,11 +6,38 @@ export default class Card {
   }
 
   _setEventListeners() {
-    alert("test alert");
+    this._cardElement
+      .querySelector(".card__favorite-button")
+      .addEventListener("click", () => {
+        this._handleFavIcon();
+      });
+    this._cardElement
+      .querySelector(".card__delete-button")
+      .addEventListener("click", () => {
+        this._handleDelIcon();
+      });
+    //
+  }
+
+  _handleFavIcon() {
+    this._cardElement
+      .querySelector(".card__favorite-button")
+      .classList.toggle("card__favorite-button_pressed");
+  }
+
+  _handleDelIcon() {
+    this._cardElement.remove();
+    this._cardElement = null;
   }
 
   getView() {
-    //getCardEl
+    this._cardElement = document
+      .querySelector(this._cardSelector)
+      .content.querySelector(".card")
+      .cloneNode(true);
+
+    //getCardEl (view)
     this._setEventListeners();
+    //return card
   }
 }
