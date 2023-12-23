ea3ef0ad3d316b8f9fc6ef6d21a74d3b594edce8
Yury Bursian <dab180996@gmail.com>
2023-12-23 14:05:25 +0200

    Popup.js done

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,18 +1,32 @@
 export default class Popup {
   constructor({ popupSelector }) {
     this._popupElement = document.querySelector(popupSelector);
+  }
 
-    // open() {
+  open() {
+    this._popupElement.classList.add("modal_opened");
+    document.addEventListener("keydown", this._handleEscClose);
+  }
 
-    // }
-    // close() {
+  close() {
+    this._popupElement.classList.add("modal_opened");
+    document.removeEventListener("keydown", this._handleEscClose);
+  }
 
-    // }
-    // _handleEscClose() {
+  _handleEscClose(evt) {
+    if (evt.key === "Escape") {
+      this.close();
+    }
+  }
 
-    // }
-    // setEventListeners() {
-
-    // }
+  setEventListeners() {
+    this._popupElement.addEventListener("click", (evt) => {
+      if (
+        evt.target === this._popupElement ||
+        evt.target.classList.contains("modal__close-button")
+      ) {
+        this.close();
+      }
+    });
   }
 }
