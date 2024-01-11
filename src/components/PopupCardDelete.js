7fc584004cb994ab2bb6edb90a4fa16b66889f3b
Yury Bursian <dab180996@gmail.com>
2024-01-11 14:40:41 +0200

    PopupCardDelete done

---
+++ src/components/PopupCardDelete.js
@@ -0,0 +1,32 @@
+import Popup from "./Popup.js";
+
+export default class PopupCardDelete extends Popup {
+  constructor({ popupSelector }) {
+    super({ popupSelector });
+    this._popupForm = this._popupElement.querySelector(".modal__form");
+    //   console.log(this._popupForm);
+    this._modalDeleteButton = this._popupElement.querySelector(
+      ".modal__save-button"
+    );
+  }
+
+  setSubmitAction(action) {
+    this._handleFormSubmit = action;
+  }
+
+  defaultText() {
+    this._modalDeleteButton.textContent = "Yes";
+  }
+
+  deleteText() {
+    this._modalDeleteButton.textContent = "Deleting...";
+  }
+
+  setEventListeners() {
+    this._popupForm.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this._handleFormSubmit();
+    });
+    super.setEventListeners();
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Card from "../components/Card.js";
 import FormValidator from "../components/FormValidator.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import PopupWithImage from "../components/PopupWithImage.js";
+import PopupCardDelete from "../components/PopupCardDelete.js";
 import Section from "../components/Section.js";
 import UserInfo from "../components/UserInfo.js";
 import { initialCards, settings } from "../utils/constants.js";
